68090a591f5825cdd279d1d0009436b5851b73b4
Gabriele <AppoggioInformatico@gmail.com>
2021-05-11 12:11:04 +0200

    make it better

--- hallo.py
+++ hallo.py
@@ -4,8 +4,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
-
-
-if __name__ == "__main__":
     app.run(ssl_context=('cert.pem','key.pem')) 
+    return render_template('index.html')
